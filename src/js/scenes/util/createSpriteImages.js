f76217bb9c9b9cfdc07be4bc551b8edc165257aa
isabellkGit <32433038+isabellkGit@users.noreply.github.com>
2018-10-26 14:07:08 -0400

    Updated buildings and royalty sprites with new images

--- src/js/scenes/util/createSpriteImages.js
+++ src/js/scenes/util/createSpriteImages.js
@@ -11,6 +11,8 @@ var attackSFrames = [29, 34, 39, 44];
 var dieNNEFrames = [45, 50, 55];
 var dieESEFrames = [48, 53, 58];
 
+var unitSprites = ['archer', 'priest', 'catapult', 'villager', 'royalty', 'swordsman', 'miner'];
+
 
 //load in the unit sprites
 function createUnitSprites(scene){
@@ -35,7 +37,7 @@ function createUnitSprites(scene){
 
   //NEEDS ROYALTY IMAGE
   scene.load.spritesheet('royalty',
-   'Graphics/units/priest.png',
+   'Graphics/units/royalty.png',
     { frameWidth: 72, frameHeight: 72}
   );
 
@@ -52,21 +54,21 @@ function createUnitSprites(scene){
 //load in the structure sprites - NEED THE IMAGES STILL
 function createStructureSprites(scene){
   scene.load.spritesheet('archeryRange',
-       'Graphics/buildings/gold_mine.png',
-       { frameWidth: 96, frameHeight: 96}
+       'Graphics/buildings/archery.png',
+       { frameWidth: 128, frameHeight: 128}
    );
    scene.load.spritesheet('barracks',
-        'Graphics/buildings/gold_mine.png',
-        { frameWidth: 96, frameHeight: 96}
+        'Graphics/buildings/barracks.png',
+        { frameWidth: 128, frameHeight: 128}
     );
 
   scene.load.spritesheet('castle',
-       'Graphics/buildings/gold_mine.png',
-       { frameWidth: 96, frameHeight: 96}
+       'Graphics/buildings/castle.png',
+       { frameWidth: 160, frameHeight: 160}
    );
   scene.load.spritesheet('machinery',
       'Graphics/buildings/gold_mine.png',
-      { frameWidth: 96, frameHeight: 96}
+      { frameWidth: 128, frameHeight: 128}
   );
 
   scene.load.spritesheet('mine',
@@ -75,12 +77,12 @@ function createStructureSprites(scene){
   );
 
   scene.load.spritesheet('temple',
-    'Graphics/buildings/gold_mine.png',
-    { frameWidth: 96, frameHeight: 96}
+    'Graphics/buildings/temple.png',
+    { frameWidth: 128, frameHeight: 128}
   );
   scene.load.spritesheet('townCenter',
     'Graphics/buildings/gold_mine.png',
-    { frameWidth: 96, frameHeight: 96}
+    { frameWidth: 128, frameHeight: 128}
   );
 }
 
@@ -97,12 +99,25 @@ function createAnim(newKeyName, originalKey, framesArr, scene){
   });
 }
 
-//IN PROGRESS NEED TO ADD THE REST
 function createAnims(scene, type){
-  createAnim(type+'WalkN', 'archer', walkNFrames, scene);
-  createAnim(type+'WalkNE', 'archer', walkNEFrames, scene);
-  createAnim(type+'WalkE', 'archer', walkEFrames, scene);
+  createAnim(type+'WalkN', type, walkNFrames, scene);
+  createAnim(type+'WalkNE', type, walkNEFrames, scene);
+  createAnim(type+'WalkE', type, walkEFrames, scene);
+  createAnim(type+'WalkSE', type, walkSEFrames, scene);
+  createAnim(type+'WalkS', type, walkSFrames, scene);
+  createAnim(type+'AttackN', type, attackNFrames, scene);
+  createAnim(type+'AttackNE', type, attackNEFrames, scene);
+  createAnim(type+'AttackE', type, attackEFrames, scene);
+  createAnim(type+'AttackSE', type, attackSEFrames, scene);
+  createAnim(type+'AttackS', type, attackSFrames, scene);
+  createAnim(type+'DieNNE', type, dieNNEFrames, scene);
+  createAnim(type+'DieESE', type, dieESEFrames, scene);
 }
+
+//goes through the list of unitSprites and creates the different animations
 function createUnitAnims(scene){
-  createAnims(scene, 'archer');
+
+  for(var i = 0; i < unitSprites.length; i++){
+    createAnims(scene, unitSprites[i]);
+  }
 }
